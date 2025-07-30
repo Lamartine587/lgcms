fc8abcff7a52e3bef5d673448561d2e75e988dbc
Lamartine587 <kipkoechlamartine@gmail.com>
2025-07-30 13:32:21 +0300

    last2

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use((req, res, next) => {
     'Content-Security-Policy',
     "default-src 'self'; " +
     "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " + // 'unsafe-inline' and 'unsafe-eval' might be needed for some dev tools or older libraries, consider removing in production
-    "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; " + // ALLOW STYLESHEETS FROM FONT AWESOME CDN AND GOOGLE FONTS
+    "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css; " + // ALLOW STYLESHEETS FROM FONT AWESOME CDN AND GOOGLE FONTS
     "img-src 'self' data: http://localhost:5001; " + // Allow images from self and your ML backend
     "connect-src 'self' http://localhost:5001; " + // ALLOW CONNECTION TO YOUR PYTHON ML BACKEND
-    "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; " + // Allow fonts from Font Awesome CDN and Google Fonts
+    "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com ; " + // Allow fonts from Font Awesome CDN and Google Fonts
     "object-src 'none'; " +
     "base-uri 'self';"
   );
